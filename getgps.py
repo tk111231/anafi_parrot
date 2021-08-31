460a91402e0372760a63c220abe16598ebe09363
tk111231 <87691649+tk111231@users.noreply.github.com>
2021-08-31 12:38:29 +0900

    Add files via upload

---
+++ getgps.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function  # python2/3 compatibility for the print function
+import olympe
+from olympe.messages.ardrone3.PilotingState import PositionChanged
+from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
+from olympe.messages.ardrone3.Piloting import TakeOff
+from olympe.messages.ardrone3.GPSSettingsState import HomeChanged
+
+# Connection
+drone = olympe.Drone("10.202.0.1")
+drone.connection()
+
+# Wait for GPS fix
+drone(GPSFixStateChanged(_policy = 'wait'))
+
+print("GPS position before take-off :", drone.get_state(HomeChanged))
+    
+
+# Take-off
+drone(TakeOff()).wait()
+
+print("GPS position after take-off : ", drone.get_state(PositionChanged))
+
+drone.disconnection()
---
+++ landing.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/python3
+
+import olympe
+import time
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+
+DRONE_IP = "192.168.42.1"
+def main():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    assert drone(Landing()).wait().success()
+    time.sleep(10)
+def camera():
+    pass
+
+
+if __name__ == "__main__":
+    main()
